9b8dcf69e6dbf11b6864638ac2012b4621b4f8bb
Admin <admin@Admins-MacBook-Pro.local>
2024-09-08 22:29:13 +0530

    Payemnt successfull dialog completed

--- src/flows/studentDashboard/dialogs/PaymentConfirmationDialog.tsx
+++ src/flows/studentDashboard/dialogs/PaymentConfirmationDialog.tsx
@@ -29,7 +29,7 @@ const PaymentConfirmationDialog = ({
       onClose={onClose}
       sx={{
         "& .MuiDialog-paper": {
-          width: 480,
+          width: 460,
           maxWidth: "50vw",
           height: 620,
           borderRadius: 3,
@@ -49,7 +49,7 @@ const PaymentConfirmationDialog = ({
         >
           <CloseIcon />
         </IconButton>
-        <Stack justifyContent="center" alignItems="center" spacing={4} pt={6}>
+        {/* <Stack justifyContent="center" alignItems="center" spacing={4} pt={6}>
           <Stack justifyContent="center" alignItems="center">
             <Typography sx={{ fontSize: 21 }}>Sent rs 5000 to</Typography>
             <Typography sx={{ fontSize: 21 }}>Suneel Satpal</Typography>
@@ -97,6 +97,68 @@ const PaymentConfirmationDialog = ({
           <Link href="#" sx={{ color: "grey", fontSize: 18 }}>
             Create another
           </Link>
+        </Stack> */}
+        <Stack justifyContent="center" alignItems="center" spacing={4} pt={5}>
+          <Stack justifyContent="center" alignItems="center" spacing={3}>
+            <Typography fontSize={24} fontWeight={550}>
+              Payment Successfull!
+            </Typography>
+            <Stack justifyContent="center" alignItems="center" spacing={5}>
+              <Stack justifyContent="center" alignItems="center">
+                <Typography align="center" fontWeight={550} fontSize={19}>
+                  Sent Rs 5000 to
+                </Typography>
+                <Typography fontWeight={550} align="center" fontSize={19}>
+                  Suneel Satpal
+                </Typography>
+              </Stack>
+              <Box>
+                <img
+                  src={TickMark}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                />
+              </Box>
+            </Stack>
+            <Stack justifyContent="center" alignItems="center">
+              <Typography align="center" fontSize={14}>
+                settlement on 7th January at 5:00pm
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack spacing={2} width="85%">
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              display="flex"
+            >
+              <Typography sx={{ fontSize: 13 }}>Transaction ID</Typography>
+              <Typography sx={{ fontSize: 13 }}>1FEDA785CB576A90</Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography sx={{ fontSize: 13 }}>Account Number</Typography>
+              <Typography sx={{ fontSize: 13 }}>1FEDA785CB576A90</Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography sx={{ fontSize: 13 }}>Account Holder</Typography>
+              <Typography sx={{ fontSize: 13 }}>1FEDA785CB576A90</Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography sx={{ fontSize: 13 }}>
+                {" "}
+                Transaction date and time
+              </Typography>
+              <Typography sx={{ fontSize: 13 }}>1FEDA785CB576A90</Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography sx={{ fontSize: 13 }}>
+                Tuition date and time
+              </Typography>
+              <Typography sx={{ fontSize: 13 }}>1FEDA785CB576A90</Typography>
+            </Stack>
+          </Stack>
         </Stack>
       </DialogContent>
     </Dialog>
