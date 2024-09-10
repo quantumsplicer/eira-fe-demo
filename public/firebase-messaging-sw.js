0ea5e13fbca6f8659e380addab2f5de1fd8fcc88
quantumsplicer <manuseth30@gmail.com>
2024-09-10 12:27:22 +0530

    notifs added

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,24 +1,26 @@
-// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCbLGYutsfYJVZCUIxvLiosgQp3xyg2u_c",
-//   authDomain: "amaranth-platform-notifs.firebaseapp.com",
-//   projectId: "amaranth-platform-notifs",
-//   storageBucket: "amaranth-platform-notifs.appspot.com",
-//   messagingSenderId: "237286150666",
-//   appId: "1:237286150666:web:06c32c5b9070df3d6185f3"
-// };
+const firebaseConfig = {
+    apiKey: "AIzaSyCbLGYutsfYJVZCUIxvLiosgQp3xyg2u_c",
+    authDomain: "amaranth-platform-notifs.firebaseapp.com",
+    projectId: "amaranth-platform-notifs",
+    storageBucket: "amaranth-platform-notifs.appspot.com",
+    messagingSenderId: "237286150666",
+    appId: "1:237286150666:web:06c32c5b9070df3d6185f3"
+  };
 
-// const messaging = firebase.messaging();
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
 
-// messaging.onBackgroundMessage(function(payload) {
-//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//   const notificationTitle = payload.notification.title;
-//   const notificationOptions = {
-//     body: payload.notification.body,
-//     icon: '/logo192.png'
-//   };
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png'
+  };
 
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// });
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
